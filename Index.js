0f21b2bf9283d21b5f529a89f9d30076ab081625
Eugenio_P <epich70@mail.ru>
2023-10-20 14:58:22 +0300

    Первойе применение функции

--- Index.js
+++ Index.js
@@ -1,29 +1,7 @@
 const myCity = {
-  city: {
-    mainCity: "Moscow",
-    bigCityOne: "Sankt Peterburg",
-    bigCityTwo: "Rostov",
+  city: "Paris",
+  cityGreeting: function () {
+    console.log("Wellcome!!");
   },
-  country: "Russia",
-  popular: true,
 };
-myCity.pi = 125;
-delete myCity.city["bigCityTwo"];
-console.log(myCity);
-
-const Mname = "Karl";
-const postsQny = 101;
-const userProfile = {
-  Mname: Mname,
-  postsQny: postsQny,
-  pet: false,
-};
-console.log(userProfile);
-//Комментарий 1//
-const Fname = "Karla";
-const postsQny1 = 11;
-const userProfileTwo = {
-  Fname,
-  postsQny1,
-};
-console.log(userProfileTwo);
+myCity.cityGreeting();
